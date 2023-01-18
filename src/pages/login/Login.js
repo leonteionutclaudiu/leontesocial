928a1f433f10f333bef6592ba38a9cac8ac7f06a
Leonte Ionut-Claudiu <leonteionut98@yahoo.com>
2023-01-18 21:04:04 +0200

    Add forgot password feature

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,6 @@
-import { Diversity2 } from '@mui/icons-material';
+import { Diversity2, LoginOutlined } from '@mui/icons-material';
 import React, { useState } from 'react';
+import { Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import classes from './Login.module.css';
 
@@ -19,6 +20,8 @@ function Login() {
   // React States
   const [errorMessages, setErrorMessages] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const [forgot, setForgot] = useState(false);
 
   const handleSubmit = (event) => {
     //Prevent page reload
@@ -48,72 +51,83 @@ function Login() {
   };
 
   const forgotHandler = () => {
-    alert(
-      'If you entered a correct email, you received an email to reset the password!'
-    );
+    setForgot(true);
+    setTimeout(() => {
+      setForgot(false);
+    }, 5000);
   };
 
   // JSX code for login form
   const renderForm = (
-    <div className={classes.form}>
-      <div className={classes.logoAndName}>
-        <Diversity2 className={classes.logoIcon} />
-        <span className={classes.logo}>LeonteSocial</span>
-      </div>
-
-      <form onSubmit={handleSubmit}>
-        <div className={classes.inputContainer}>
-          <input
-            className={classes.loginInput}
-            type="email"
-            name="email"
-            required
-            placeholder="Email"
-          />
-        </div>
-        <div className={classes.inputContainer}>
-          <input
-            className={classes.loginInput}
-            type="password"
-            name="pass"
-            required
-            placeholder="Password"
-          />
-        </div>
-        <div className={classes.buttonContainer}>
-          <button
-            className={classes.loginBtn}
-            onClick={errorHandler}
-            type="submit"
-          >
-            Log in
-          </button>
-          <Link to="/register" className={classes.loginRegisterButton}>
-            Sign up
-          </Link>
-          <div className={classes.forgot} onClick={forgotHandler}>
-            Forgot your password?
+    <div className={classes.login}>
+      <div className={classes.loginForm}>
+        <div className={classes.form}>
+          <div className={classes.logoAndName}>
+            <Diversity2 className={classes.logoIcon} />
+            <span className={classes.logo}>LeonteSocial</span>
           </div>
+
+          <form onSubmit={handleSubmit}>
+            <div className={classes.inputContainer}>
+              <input
+                className={classes.loginInput}
+                type="email"
+                name="email"
+                required
+                placeholder="Email"
+              />
+            </div>
+            <div className={classes.inputContainer}>
+              <input
+                className={classes.loginInput}
+                type="password"
+                name="pass"
+                required
+                placeholder="Password"
+              />
+            </div>
+            <div className={classes.buttonContainer}>
+              <button
+                className={classes.loginBtn}
+                onClick={errorHandler}
+                type="submit"
+              >
+                Log in
+              </button>
+              <Link to="/register" className={classes.loginRegisterButton}>
+                Sign up
+              </Link>
+              <div className={classes.forgot} onClick={forgotHandler}>
+                Forgot your password?
+              </div>
+              {forgot ? (
+                <Alert className={classes.alert}>
+                  If you entered a correct email, you received an email to reset
+                  the password!
+                </Alert>
+              ) : (
+                ''
+              )}
+              <span className={classes.error}>{errorMessages}</span>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 
   return (
-    <div className={classes.login}>
-      <div className={classes.loginForm}>
-        {isSubmitted ? (
-          <>
-            <Link to="/home" className={classes.loginNow}>
-              I'm sure, I want to Log in!
-            </Link>
-          </>
-        ) : (
-          renderForm
-        )}
-        <span className={classes.error}>{errorMessages}</span>
-      </div>
-    </div>
+    <>
+      {isSubmitted ? (
+        <div className={classes.imSure}>
+          <Link to="/home">
+            <LoginOutlined className={classes.loginNow} />
+          </Link>
+        </div>
+      ) : (
+        renderForm
+      )}
+    </>
   );
 }
 
